fcc834e1ba32dc08458651e009026bfdbcce14cd
NightFog996 <yjhtester@gmail.com>
2021-08-03 22:46:38 +0800

    add new httprunner3

---
+++ new_httprunner3/test.py
@@ -0,0 +1,17 @@
+
+
+# import os 
+# pd1 = os.getcwd()
+# os.chdir(pd1+'/httprunner')
+# print(os.getcwd())
+from httprunner.cli import tttest,main
+print('test start')
+
+
+# tttest()
+main()
+
+"""
+
+如果是在最里面的httprunner文件夹运行test，则提示
+"""
